bd78e71a7d2c9986f5c181437443e7a066610dd1
Nate Mara <natemara@gmail.com>
2014-03-11 14:03:42 -0400

    added json file io

--- any_do.py
+++ any_do.py
@@ -1,23 +1,33 @@
 from anydo.api import AnyDoAPI
 from anydo.error import AnyDoAPIError
 from getpass import getpass
+import json
 
 def main():
-	print_tasks(get_task_list())
+	user_info = get_un_pw()
+	api = get_api(user_info)
+	task_list = get_task_list(api)
+	print_tasks(task_list)
 
-def get_task_list():
+def prompt_for_api():
 	username = input('What is your Any.do username? ')
 	password = getpass('What is your Any.do password? ')
 
 	if '@' not in username:
 		username += '@gmail.com'
 
+	return (username, password)
+
+def get_api(user_info):
 	try:
-		api = AnyDoAPI(username, password)
-		return api.get_all_tasks()
+		api = AnyDoAPI(user_info[0], user_info[1])
+		return api
 	except AnyDoAPIError:
 		print('Invalid login')
-		return get_task_list()
+		return prompt_for_api()
+
+def get_task_list(api):
+	return api.get_all_tasks()
 
 def print_tasks(task_list):
 	print()
@@ -27,6 +37,19 @@ def print_tasks(task_list):
 		task_names[i] = str(i + 1) + '. ' + task_names[i]
 	print('\n'.join(task_names))
 	print()
+
+def get_un_pw():
+	"""
+	Parses the data file anydo.json for a user's saved info
+	and returns a tuple containing the username and password
+	from said data file.
+	"""
+	try:
+		with open('anydo.json') as json_file:
+			user_info = json.load(json_file)
+		return (user_info['username'], user_info['password'])
+	except FileNotFoundError:
+		return prompt_for_api()
 
 if __name__ == '__main__':
 	main()
